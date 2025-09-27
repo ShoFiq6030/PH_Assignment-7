9851f6800f9200ddd0e53c9869828e8766bbde24
shofiq6030 <mdshofiq6030@gmail.com>
2025-09-27 17:32:40 +0600

    fixed- dashboard bg img

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ export default function Dashboard({ inProgressData, resolvedTaskData }) {
     <div className="flex py-10 lg:gap-10 gap-2 container mx-auto ">
       <div className="w-1/2 max-h-[250px]  bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white">
         <div className="flex justify-between ">
-          <div className="bg-[url(./src/assets/vector1.png)] max-h-[250px] bg-no-repeat w-80 h-80"></div>
+          <div className="bg-[url(https://i.ibb.co.com/qM4YkCrt/vector1.png)] max-h-[250px] bg-no-repeat w-80 h-80"></div>
           <div className="flex flex-col justify-center items-center gap-4">
             <span className="text-xl text-nowrap">In-Progress</span>
             <p className="text-5xl font-bold">{inProgressData.length}</p>
@@ -15,7 +15,7 @@ export default function Dashboard({ inProgressData, resolvedTaskData }) {
       </div>
       <div className="w-1/2 max-h-[250px]  bg-linear-to-r from-[#54CF68] to-[#00827A] text-white">
         <div className="flex justify-between ">
-          <div className="bg-[url(./src/assets/vector1.png)] max-h-[250px] bg-no-repeat w-80 h-80"></div>
+          <div className="bg-[url(https://i.ibb.co.com/qM4YkCrt/vector1.png)] max-h-[250px] bg-no-repeat w-80 h-80"></div>
           <div className="flex flex-col justify-center items-center gap-4">
             <span className="text-xl text-nowrap">Resolved</span>
             <p className="text-5xl font-bold">{resolvedTaskData.length}</p>
